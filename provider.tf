da94e7b464154fa5fd9444c2667462cd93c53195
vinaybv <vinay.bv@rapyder.com>
2025-04-24 14:54:00 +0530

    backend added

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "us-east-1"
 }
+terraform {
+  backend "s3" {
+    bucket         = "vinay-deep-s3-terraform-buvket"
+    key            = "terraform/eks/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+  
