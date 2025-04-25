a7c38ba27b0e64e72db076e3a66b7293c13a238d
vinaybv <vinay.bv@rapyder.com>
2025-04-25 14:56:17 +0530

    update

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ module "eks" {
   cluster_version = "1.32"
 
   cluster_endpoint_public_access = true
-  enable_cluster_creator_admin_permissions = true
+  enable_cluster_creator_admin_permissions = true7
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
